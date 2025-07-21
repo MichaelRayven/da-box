f371ef8bdbf0e2979992b2b6f42016ffbf88aa3b
Michael Rayven <themikerayven@gmail.com>
2025-07-22 05:10:29 +0700

    Use Link in folder navigation

--- src/components/file-list.tsx
+++ src/components/file-list.tsx
@@ -1,9 +1,4 @@
-"use client";
-
 import { Folder } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { mockFiles, mockFolders } from "~/app/_mock";
 import {
   Table,
   TableBody,
@@ -11,38 +6,13 @@ import {
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
-import type { File as IFile, Folder as IFolder } from "~/lib/interface";
 import { FileRow, FolderRow } from "./file-row";
+import type { files, folders } from "~/server/db/schema";
 
-export default function FileList() {
-  const router = useRouter();
-  const params = useParams<{ folderId: string | undefined }>();
-  const currentFolderId = params.folderId || "root";
-
-  const [currentFolders, setCurrentFolders] = useState<IFolder[]>([]);
-  const [currentFiles, setCurrentFiles] = useState<IFile[]>([]);
-
-  useEffect(() => {
-    const filteredFiles = mockFiles.filter(
-      (item) => item.parent == currentFolderId
-    );
-
-    const filteredFolders = mockFolders.filter(
-      (item) => item.parent == currentFolderId
-    );
-
-    setCurrentFiles(filteredFiles);
-    setCurrentFolders(filteredFolders);
-  }, [currentFolderId]);
-
-  const handleFolderClick = (item: IFolder) => {
-    router.push(`/drive/folders/${item.id}`);
-  };
-
-  const handleFileClick = (item: IFile) => {
-    window.open(item.url, "_blank");
-  };
-
+export default function FileList(props: {
+  files: (typeof files.$inferSelect)[];
+  folders: (typeof folders.$inferSelect)[];
+}) {
   return (
     <div className="bg-gray-800 rounded-lg border border-gray-700">
       <Table>
@@ -55,27 +25,15 @@ export default function FileList() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {currentFolders.map((item) => (
-            <FolderRow
-              folder={item}
-              key={item.id}
-              handleFolderClick={() => {
-                handleFolderClick(item);
-              }}
-            />
+          {props.folders.map((item) => (
+            <FolderRow folder={item} key={item.id} />
           ))}
-          {currentFiles.map((item) => (
-            <FileRow
-              file={item}
-              key={item.id}
-              handleFileClick={() => {
-                handleFileClick(item);
-              }}
-            />
+          {props.files.map((item) => (
+            <FileRow file={item} key={item.id} />
           ))}
         </TableBody>
       </Table>
-      {currentFiles.length === 0 && currentFolders.length === 0 && (
+      {props.files.length === 0 && props.folders.length === 0 && (
         <div className="text-center py-12">
           <Folder className="mx-auto h-12 w-12 text-gray-500 mb-4" />
           <h3 className="text-lg font-medium text-gray-300 mb-2">
--- src/components/file-row.tsx
+++ src/components/file-row.tsx
@@ -1,51 +1,39 @@
 import { FileText, Folder } from "lucide-react";
+import Link from "next/link";
 import { TableCell, TableRow } from "~/components/ui/table";
-import type { File as IFile, Folder as IFolder } from "~/lib/interface";
+import type { files, folders } from "~/server/db/schema";
 
-export function FileRow({
-  file,
-  handleFileClick = () => {},
-}: {
-  file: IFile;
-  handleFileClick?: () => void;
-}) {
+export function FileRow({ file }: { file: typeof files.$inferSelect }) {
   return (
-    <TableRow
-      className="border-gray-700 hover:bg-gray-750 cursor-pointer"
-      onClick={handleFileClick}
-    >
-      <TableCell className="text-white hover:text-blue-400">
-        <div className="flex items-center space-x-3">
+    <TableRow className="border-gray-700 hover:bg-gray-750 cursor-pointer">
+      <TableCell className="p-0 text-white hover:text-blue-400">
+        <Link href={file.url} className="p-2 flex items-center space-x-3">
           <FileText className="size-6" />
           <span>{file.name}</span>
-        </div>
+        </Link>
       </TableCell>
-      <TableCell className="text-gray-300">{file.owner}</TableCell>
-      <TableCell className="text-gray-300">{file.modified}</TableCell>
+      <TableCell className="text-gray-300">{/*file.owner*/}</TableCell>
+      <TableCell className="text-gray-300">
+        {file.modified?.toDateString()}
+      </TableCell>
       <TableCell className="text-gray-300">{file.size || "—"}</TableCell>
     </TableRow>
   );
 }
 
-export function FolderRow({
-  folder,
-  handleFolderClick = () => {},
-}: {
-  folder: IFolder;
-  handleFolderClick?: () => void;
-}) {
+export function FolderRow({ folder }: { folder: typeof folders.$inferSelect }) {
   return (
-    <TableRow
-      className="border-gray-700 hover:bg-gray-750 cursor-pointer"
-      onClick={handleFolderClick}
-    >
-      <TableCell className="text-white hover:text-blue-400">
-        <div className="flex items-center space-x-3">
+    <TableRow className="border-gray-700 hover:bg-gray-750 cursor-pointer">
+      <TableCell className="p-0 text-white hover:text-blue-400">
+        <Link
+          className="p-2 flex items-center space-x-3"
+          href={`/drive/folders/${folder.id}`}
+        >
           <Folder className="size-6" />
           <span>{folder.name}</span>
-        </div>
+        </Link>
       </TableCell>
-      <TableCell className="text-gray-300">{folder.owner}</TableCell>
+      <TableCell className="text-gray-300">{/*folder.owner*/}</TableCell>
     </TableRow>
   );
 }
