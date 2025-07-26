a5eedb3f2eb1484167e6fef48491bc4066b287bf
Michael Rayven <themikerayven@gmail.com>
2025-07-26 23:32:16 +0700

    Formatting

--- src/app/sign-in/form.tsx
+++ src/app/sign-in/form.tsx
@@ -7,91 +7,91 @@ import { useForm } from "react-hook-form";
 import type { z } from "zod";
 import { Button } from "~/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
 import { cn } from "~/lib/utils";
 import { signInSchema } from "~/lib/validation";
 
 interface SignInFormProps {
-  id?: string;
-  className?: string;
-  showSubmit?: boolean;
+	id?: string;
+	className?: string;
+	showSubmit?: boolean;
 }
 
 export function SignInForm({
-  id,
-  className,
-  showSubmit = true,
+	id,
+	className,
+	showSubmit = true,
 }: SignInFormProps) {
-  const form = useForm<z.infer<typeof signInSchema>>({
-    resolver: zodResolver(signInSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+	const form = useForm<z.infer<typeof signInSchema>>({
+		resolver: zodResolver(signInSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
 
-  const onSubmit = (values: z.infer<typeof signInSchema>) => {
-    signIn("credentials", { ...values });
-  };
+	const onSubmit = (values: z.infer<typeof signInSchema>) => {
+		signIn("credentials", { ...values });
+	};
 
-  return (
-    <Form {...form}>
-      <form
-        id={id}
-        onSubmit={form.handleSubmit(onSubmit)}
-        className={cn("flex flex-col gap-8", className)}
-      >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  type="email"
-                  aria-required="true"
-                  autoComplete="email"
-                  placeholder="email@example.com ..."
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input
-                  type="password"
-                  aria-required="true"
-                  autoComplete="password"
-                  placeholder="Enter your password ..."
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {showSubmit && (
-          <Button type="submit" className="w-1/2 self-center">
-            <LogInIcon /> Sign In
-          </Button>
-        )}
-      </form>
-    </Form>
-  );
+	return (
+		<Form {...form}>
+			<form
+				id={id}
+				onSubmit={form.handleSubmit(onSubmit)}
+				className={cn("flex flex-col gap-8", className)}
+			>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input
+									type="email"
+									aria-required="true"
+									autoComplete="email"
+									placeholder="email@example.com ..."
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Password</FormLabel>
+							<FormControl>
+								<Input
+									type="password"
+									aria-required="true"
+									autoComplete="password"
+									placeholder="Enter your password ..."
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{showSubmit && (
+					<Button type="submit" className="w-1/2 self-center">
+						<LogInIcon /> Sign In
+					</Button>
+				)}
+			</form>
+		</Form>
+	);
 }
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,14 +1,14 @@
 import { SignInForm } from "./form";
 
 export default function SignInPage() {
-  return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="md:-mt-32 mx-auto flex w-full max-w-[400px] flex-col gap-8">
-        <SignInForm />
-        {/* <span className="w-full flex items-center gap-4">
+	return (
+		<main className="flex items-center justify-center md:h-screen">
+			<div className="md:-mt-32 mx-auto flex w-full max-w-[400px] flex-col gap-8">
+				<SignInForm />
+				{/* <span className="w-full flex items-center gap-4">
           <hr className="flex-1 h-px" /> or <hr className="flex-1 h-px" />
         </span> */}
-      </div>
-    </main>
-  );
+			</div>
+		</main>
+	);
 }
