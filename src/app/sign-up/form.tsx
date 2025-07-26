73798a5840e017c719e5deb9f8b137fb1b918e93
Michael Rayven <themikerayven@gmail.com>
2025-07-26 23:37:45 +0700

    send signup request

--- src/app/sign-up/form.tsx
+++ src/app/sign-up/form.tsx
@@ -5,135 +5,142 @@ import { useForm } from "react-hook-form";
 import type { z } from "zod";
 import { Button } from "~/components/ui/button";
 import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
 import { cn } from "~/lib/utils";
 import { signUpSchema } from "~/lib/validation";
 
 interface SignUpFormProps {
-	id?: string;
-	className?: string;
-	showSubmit?: boolean;
+  id?: string;
+  className?: string;
+  showSubmit?: boolean;
 }
 
 export function SignUpForm({
-	id,
-	className,
-	showSubmit = true,
+  id,
+  className,
+  showSubmit = true,
 }: SignUpFormProps) {
-	const form = useForm<z.infer<typeof signUpSchema>>({
-		resolver: zodResolver(signUpSchema),
-		defaultValues: {
-			username: "",
-			email: "",
-			password: "",
-			confirmPassword: "",
-		},
-	});
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-	const onSubmit = (values: z.infer<typeof signUpSchema>) => {
-		// const { isPending, error, data } = useQuery({
-		//   queryKey: ["repoData"],
-		//   queryFn: () => fetch("/api/auth/sign-in").then((res) => res.json()),
-		// });
-	};
+  const onSubmit = async (values: z.infer<typeof signUpSchema>) => {
+    // const { isPending, error, data } = useQuery({
+    //   queryKey: ["repoData"],
+    //   queryFn: () => fetch("/api/auth/sign-in").then((res) => res.json()),
+    // });
+    await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+  };
 
-	return (
-		<Form {...form}>
-			<form
-				id={id}
-				onSubmit={form.handleSubmit(onSubmit)}
-				className={cn("space-y-8", className)}
-			>
-				<FormField
-					control={form.control}
-					name="username"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Username *</FormLabel>
-							<FormControl>
-								<Input
-									type="text"
-									aria-required="true"
-									autoComplete="name"
-									placeholder="John ..."
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="email"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email *</FormLabel>
-							<FormControl>
-								<Input
-									type="email"
-									aria-required="true"
-									autoComplete="email"
-									placeholder="john@example.com ..."
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="password"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Password *</FormLabel>
-							<FormControl>
-								<Input
-									type="password"
-									aria-required="true"
-									autoComplete="new-password"
-									placeholder="Enter your password ..."
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="confirmPassword"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Confirm Password *</FormLabel>
-							<FormControl>
-								<Input
-									type="password"
-									aria-required="true"
-									autoComplete="new-password"
-									placeholder="Confirm password ..."
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				{showSubmit && (
-					<Button type="submit" variant="secondary">
-						Submit
-					</Button>
-				)}
-			</form>
-		</Form>
-	);
+  return (
+    <Form {...form}>
+      <form
+        id={id}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn("space-y-8", className)}
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username *</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  aria-required="true"
+                  autoComplete="name"
+                  placeholder="John ..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email *</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  aria-required="true"
+                  autoComplete="email"
+                  placeholder="john@example.com ..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password *</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  aria-required="true"
+                  autoComplete="new-password"
+                  placeholder="Enter your password ..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password *</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  aria-required="true"
+                  autoComplete="new-password"
+                  placeholder="Confirm password ..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {showSubmit && (
+          <Button type="submit" variant="secondary">
+            Submit
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
 }
